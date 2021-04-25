ce19d4fa9a7907aefb16ab4a4b04e56864e268c0
Ben <bftingle@ucsc.edu>
2021-04-25 02:19:39 -0700

    Cabin & Trailer

---
+++ src/prefabs/Cabin.js
@@ -0,0 +1,9 @@
+class Cabin extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        scene.add.existing(this);
+    }
+
+    
+}
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,10 +5,14 @@ class Menu extends Phaser.Scene {
 
     preload() {
         this.load.image('road', './assets/road3.png');
+        this.load.image('cabin', './assets/cabin1.png');
+        this.load.image('trailer', './assets/trailer1.png');
     }
 
     create() {
         this.road = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'road').setOrigin(0, 0);
+        this.cabin = new Cabin(this, 156, 152, 'cabin', 0).setOrigin(0, 0);
+        this.trailer = new Trailer(this, 12, 152, 'trailer', 0).setOrigin(0, 0);
     }
 
     update() {
