8abf5229a6b21e773a6161ca33e0bc279dc10164
Ben <bftingle@ucsc.edu>
2021-04-25 16:45:31 -0700

    Title

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -13,6 +13,14 @@ class Menu extends Phaser.Scene {
         this.road = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'road').setOrigin(0, 0);
         this.cabin = new Cabin(this, 156, 152, 'cabin', 0).setOrigin(0, 0);
         this.trailer = new Trailer(this, 12, 152, 'trailer', 0).setOrigin(0, 0);
+
+        let menuStyle = {
+            fontFamily: 'Righteous',
+            fontSize: '60px',
+            color: '#FFFFFF',
+            align: 'right'
+        }
+        this.add.text(324, 90, 'Egg Scramble', menuStyle).setOrigin(0.5);
     }
 
     update() {
